8ec3d34b70d80c19f5ce36cef175979e2b938b1f
RAHUL SHAHI <rahulshahi@RAHULs-Air.lan>
2021-09-23 22:16:06 -0400

    UI tables

--- client/src/login.js
+++ client/src/login.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios'
 import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
+import _ from "lodash";
 import "./login.css";
 
 export default function Login() {
@@ -19,6 +20,15 @@ const fetchAPI = async (param) => {
   .catch(err => console.log("My error, ", err));
 }
 
+const fetchallAPI = async () => {
+  // param is a highlighted word from the user before it clicked the button 
+  const res = await fetch("http://localhost:5000/lsds/covidTally/", {
+    method: 'GET',
+    mode: 'cors'
+  }).then(res => res.json()).then(dat => setData(dat))
+  .catch(err => console.log("My error, ", err));
+}
+
 useEffect(() => {
   console.log("Data");
   console.log(data);
@@ -29,7 +39,17 @@ const handleSubmit = (event) => {
 }
 
 return (
+  
   <div className="Login">
+
+  <div class="header">
+  <a href="#default" class="logo">COVID-19 TRACKER</a>
+  <div class="header-right">
+    <a class="active" href="#home">Home</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+  </div>
+</div>
     <Form onSubmit={handleSubmit}>
       <Form.Group size="lg" controlId="email">
         <Form.Label>Country</Form.Label>
@@ -41,17 +61,22 @@ return (
         />
         <br></br>
       </Form.Group>
-      <Button onClick={() => fetchAPI(email)}>
-        Search
-      </Button>
+      <div className="btn-wrapper">
+        <Button onClick={() => fetchAPI(email)}>
+          Search
+        </Button>
+        <Button onClick={() => fetchallAPI()}>
+          All Countries
+        </Button>
+      </div>
+      
     </Form>
 
 <div>
   <table className="taglist">
+  
     <tbody>
-    {data && data.map(({newDocument}) => (
-      <div>
-      <tr>
+    <tr>
         <th>CODE</th>
         <th>COUNTRY</th>
         <th>CONFIRMED</th>
@@ -60,6 +85,7 @@ return (
         <th>RECOVERED</th>
         <th>LASTUPDATE</th>
       </tr>
+    {data && _.isArray(data) ? data.map(({newDocument}) => (
       
       <tr>
         <td>{newDocument.code}</td>
@@ -70,8 +96,17 @@ return (
         <td>{newDocument.recovered}</td>
         <td>{newDocument.lastupdate}</td>
       </tr>
-    </div>
-    ))}
+  
+    )): (data && <tr>
+      <td>{data.newDocument.code}</td>
+      <td>{data.newDocument.country}</td>
+      <td>{data.newDocument.confirmed}</td>
+      <td>{data.newDocument.critical}</td>
+      <td>{data.newDocument.deaths}</td>
+      <td>{data.newDocument.recovered}</td>
+      <td>{data.newDocument.lastupdate}</td>
+    </tr> )
+   }
     </tbody>
   
 </table>
